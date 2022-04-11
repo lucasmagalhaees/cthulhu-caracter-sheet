fca3156a98be591a48513805f49978fde31bf6ec
Lucas <lucasbarbosa1738@gmail.com>
2022-04-11 18:55:16 -0300

    adding age to the sheet

--- src/main/java/com/lucasbarbosa/cthulhu/caracter/sheet/model/SheetVO.java
+++ src/main/java/com/lucasbarbosa/cthulhu/caracter/sheet/model/SheetVO.java
@@ -1,70 +1,83 @@
 package com.lucasbarbosa.cthulhu.caracter.sheet.model;
 
-import static com.lucasbarbosa.cthulhu.caracter.sheet.model.CharacteristicEnum.CONSTITUTION;
-import static com.lucasbarbosa.cthulhu.caracter.sheet.model.CharacteristicEnum.POWER;
-import static com.lucasbarbosa.cthulhu.caracter.sheet.model.CharacteristicEnum.SIZE;
-import static com.lucasbarbosa.cthulhu.caracter.sheet.model.CharacteristicEnum.STRENGTH;
-import static com.lucasbarbosa.cthulhu.caracter.sheet.util.ApplicationUtils.bigDecimalGen;
-import static com.lucasbarbosa.cthulhu.caracter.sheet.util.ApplicationUtils.getCharacteristicMainValue;
-import static com.lucasbarbosa.cthulhu.caracter.sheet.util.ApplicationUtils.rollDice;
-import static java.math.RoundingMode.DOWN;
-
 import com.lucasbarbosa.cthulhu.caracter.sheet.util.ApplicationUtils;
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.lucasbarbosa.cthulhu.caracter.sheet.model.CharacteristicEnum.*;
+import static com.lucasbarbosa.cthulhu.caracter.sheet.util.ApplicationUtils.*;
+import static java.math.RoundingMode.DOWN;
 
 @Getter
 @Builder
 public class SheetVO {
 
-  private BigDecimal hitPoints;
-  private BigDecimal magicPoints;
+    private BigDecimal hitPoints;
+    private BigDecimal magicPoints;
 
-  @Setter
-  private Integer damageBonus;
+    @Setter
+    private Integer damageBonus;
 
-  @Setter
-  private Integer build;
+    @Setter
+    private Integer build;
 
-  private List<AttributeVO> characterists;
-  private List<AttributeVO> skills;
+    @Setter
+    private Integer movementRate;
 
-  public static SheetVO buildSheet(List<AttributeVO> characterists, List<AttributeVO> skills) {
+    @Setter
+    private Integer age;
 
-    var size = getCharacteristicMainValue(characterists, SIZE);
-    var strength = getCharacteristicMainValue(characterists, STRENGTH);
+    private List<AttributeVO> characterists;
+    private List<AttributeVO> skills;
 
-    var hitPoints = getCharacteristicMainValue(characterists, CONSTITUTION).add(
-        size).divide(bigDecimalGen(5), 0, DOWN);
+    public static SheetVO buildSheet(List<AttributeVO> characterists, List<AttributeVO> skills) {
 
-    var magicPoints = getCharacteristicMainValue(characterists, POWER).divide(bigDecimalGen(5), 0,
-        DOWN);
+        var size = getCharacteristicMainValue(characterists, SIZE);
+        var strength = getCharacteristicMainValue(characterists, STRENGTH);
+        var dexterity = getCharacteristicMainValue(characterists, DEXTERITY);
 
-    SheetVO sheetVO = SheetVO.builder().characterists(characterists).skills(skills)
-        .hitPoints(hitPoints)
-        .magicPoints(magicPoints)
-        .build();
+        var hitPoints = getCharacteristicMainValue(characterists, CONSTITUTION).add(
+                size).divide(bigDecimalGen(5), 0, DOWN);
 
-    var bonusParam = size.add(strength);
+        var magicPoints = getCharacteristicMainValue(characterists, POWER).divide(bigDecimalGen(5), 0,
+                DOWN);
 
-    var bonusValue = Arrays.stream(BonusEnum.values()).filter(
-        bonus -> ApplicationUtils.isBetween(bonusParam, bigDecimalGen(bonus.getMin()),
-            bigDecimalGen(bonus.getMax()))).findAny();
+        SheetVO sheetVO = SheetVO.builder().characterists(characterists).skills(skills)
+                .hitPoints(hitPoints)
+                .magicPoints(magicPoints)
+                .build();
 
-    bonusValue.ifPresent(bonus -> {
-      if (bonus.getIsSigned()) {
-        sheetVO.setDamageBonus(bonus.getSignedDamage());
-        sheetVO.setBuild(bonus.getSignedBuild());
-      }
+        var bonusParam = size.add(strength);
 
-      sheetVO.setDamageBonus(rollDice(bonus.getDieNumber(), bonus.getDieFaces()).intValue());
-      sheetVO.setBuild(bonus.getBuild());
-    });
+        var bonusValue = Arrays.stream(BonusEnum.values()).filter(
+                bonus -> ApplicationUtils.isBetween(bonusParam, bigDecimalGen(bonus.getMin()),
+                        bigDecimalGen(bonus.getMax()))).findAny();
 
-    return sheetVO;
-  }
+        bonusValue.ifPresent(bonus -> {
+            if (bonus.getIsSigned()) {
+                sheetVO.setDamageBonus(bonus.getSignedDamage());
+                sheetVO.setBuild(bonus.getSignedBuild());
+            }
+
+            sheetVO.setDamageBonus(rollDice(bonus.getDieNumber(), bonus.getDieFaces()).intValue());
+            sheetVO.setBuild(bonus.getBuild());
+        });
+
+        if (dexterity.compareTo(size) < 0 && strength.compareTo(size) < 0) {
+            sheetVO.setMovementRate(7);
+        } else if (dexterity.compareTo(size) >= 0 || strength.compareTo(size) >= 0) {
+            sheetVO.setMovementRate(8);
+        } else if (dexterity.compareTo(size) > 0 && strength.compareTo(size) > 0) {
+            sheetVO.setMovementRate(9);
+        }
+
+        sheetVO.setAge(ApplicationUtils.calculateRandomAge());
+
+        return sheetVO;
+    }
 }
