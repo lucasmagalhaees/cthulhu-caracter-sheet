f88860383d6b863c35ce2130e3c7625f7c9bb273
Lucas <lucasbarbosa1738@gmail.com>
2022-04-12 14:23:03 -0300

    testing Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,22 @@
-FROM openjdk:11
+# Compile and build the project
+FROM gradle:jdk11 as build-image
+
+VOLUME /tmp
+WORKDIR /
+ADD . .
+
+
+RUN gradle --stacktrace clean test build
 COPY build/libs/*.jar app.jar
+
+FROM openjdk:11
+
+#
+# Copy the jar file in and name it app.jar.
+#
+COPY --from=build-image app.jar /
+
+#
+# The command to run when the container starts.
+#
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/lucasbarbosa/cthulhu/character/generator/driver/config/SwaggerConfig.java
+++ src/main/java/com/lucasbarbosa/cthulhu/character/generator/driver/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Call of Cthulhu Character Generator")
+                .title("Call of Cthulhu Random Character Generator")
                 .description("Project made for randomly generating a Call of Cthulhu Character Sheet")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
